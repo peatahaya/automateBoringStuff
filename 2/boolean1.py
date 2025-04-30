1abd24f0341e77590291443a68d11f86cc90dfd1
peatahaya <piotrbi3licki@gmail.com>
2025-04-30 22:31:35 +0200

    adding boolean1.py

---
+++ 2/boolean1.py
@@ -0,0 +1,11 @@
+# simple hardcoded name & password validation program
+
+name = 'Mary'
+password = 'swordfish'
+
+if name == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish':
+        print('Access granted.')
+    else:
+        print('Wrong password.')
