e314758a54a727270ded60f9f8e2e945c224cc13
peatahaya <piotrbi3licki@gmail.com>
2025-04-30 22:40:41 +0200

    adding name.py

---
+++ 2/name.py
@@ -0,0 +1,8 @@
+# program checks valid name and prompts appropriate message.
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you!')
