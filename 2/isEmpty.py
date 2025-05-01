e9b62b1b7e30ca25db41f5cd21533018ba9bb881
peatahaya <piotrbi3licki@gmail.com>
2025-05-01 09:34:52 +0200

    adding isEmpty.py

---
+++ 2/isEmpty.py
@@ -0,0 +1,11 @@
+# program with while loop that checks if variables has value
+
+name = ''
+while not name:
+    print('Enter your name:')
+    name = input()
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests:
+    print('Be sure to have enough room for all your guests.')
+print('Done')
